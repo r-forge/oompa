95b19dde1fb9e3d899c3dea12552c897671137d9
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2022-05-05 13:45:04 +0000

    Added interpretation functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@514 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/plasma/tests/test05-interpret.R
@@ -0,0 +1,40 @@
+library(plasma)
+data("TCGA-ESCA")
+## prepare MultiOmics
+MO <- prepareMultiOmics(assemble, Outcome)
+MO <- MO[c("ClinicalBin", "ClinicalCont", "RPPA"),]
+summary(MO)
+## split into train and test
+set.seed(12345)
+train <- rep(FALSE, 185)
+train[sample(185, 113)] <- TRUE
+MO2 <- MO[, train]
+summary(MO2)
+## test complete cox models
+bigfit <- fitCoxModels(MO2, "Days", "vital_status", "dead")
+## extend across dataset pairs
+mfm <- plasma(MO2, bigfit)
+plot(mfm)
+
+cross <- getCompositeWeights(mfm, "ClinicalBin", "RPPA")
+summary(cross)
+image(cross)
+
+heat(cross)
+
+cb <- getAllWeights(mfm, "ClinicalBin")
+summary(cb)
+image(cb)
+heat(cb)
+getTop(cb)
+
+rppaContrib <- getAllWeights(mfm, "RPPA")
+getTop(rppaContrib, 5)
+image(rppaContrib)
+heat(rppaContrib)
+
+Q <- max(abs(quantile(rppaContrib@contrib, c(0.05, 0.95))))
+sig <- apply(abs(rppaContrib@contrib) > Q, 1, any)
+smaller <- rppaContrib[sig,]
+image(smaller)
+heat(smaller)
