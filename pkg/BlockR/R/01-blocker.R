f225e0a30621219afca4eaa59e077e886790ebff
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2022-08-23 14:25:25 +0000

    Working on blockr vignette.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@552 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BlockR/R/01-blocker.R
+++ pkg/BlockR/R/01-blocker.R
@@ -1,16 +1,10 @@
-blocker <- function(dataset) {
-  sampleClades <- hclust(distanceMatrix(t(dataset), metric="pearson"), 
-                         method="ward.D2")
-  proteinClades <- hclust(distanceMatrix(dataset, metric="pear"), 
-                          method="ward.D2")
-  NP <- trunc(3 + sqrt(ncol(dataset)))
-  NS <- trunc(1 + sqrt(nrow(dataset)))
+blockerLM <- function(dataset, sampleClades, proteinClades, NP, NS) {
   storage <- matrix(NA, nrow=NP*NS, ncol=3)
   idx <- 0
   for (np in 1:NP) {
-    pgroup <- paste("p", cutree(proteinClades, k=np), sep='')
+    pgroup <- paste("P", cutree(proteinClades, k=np), sep='')
     for (ns in 1:NS) {
-      sgroup <- paste("p", cutree(sampleClades, k=ns), sep='')
+      sgroup <- paste("S", cutree(sampleClades, k=ns), sep='')
       pivot <- data.frame(Y=as.vector(as.matrix(dataset)),
                           P=rep(pgroup, each=nrow(dataset)),
                           S=rep(sgroup, times=ncol(dataset)))
@@ -34,6 +28,43 @@ blocker <- function(dataset) {
   storage
 }
 
+## protein clades are columns
+## sample clades are rows
+blockerLoop <- function(dataset, sampleClades, proteinClades, NP, NS) {
+  storage <- matrix(NA, nrow=NP*NS, ncol=3)
+  idx <- 0
+  for (np in 1:NP) {
+    cat(np, "\n", file = stderr())
+    for (ns in 1:NS) {
+      RR <- factor(paste("R", cutree(sampleClades, k = ns), sep = ""))
+      CC <- factor(paste("C", cutree(proteinClades, k = np), sep = ""))
+      P <- 0*dataset
+      for (R in levels(RR)) {
+        for (C in levels(CC)) {
+          P[RR == R, CC == C] <- mean(dataset[RR==R, CC==C])
+        }
+      }
+      Res <- matrix(dataset - P, nrow = nrow(dataset))
+      idx <- idx+1
+      storage[idx,] <- c(np, ns, mean(Res^2)) # mean square residual
+    } # end cn
+  } # end rn
+  colnames(storage) <- c("ProteinGroups", "SampleGroups", "MeanMSE")
+  storage <- as.data.frame(storage)
+  storage
+}
+
+
+blocker <- function(dataset) {
+  sampleClades <- hclust(distanceMatrix(t(dataset), metric="pearson"), 
+                         method="ward.D2")
+  proteinClades <- hclust(distanceMatrix(dataset, metric="pear"), 
+                          method="ward.D2")
+  NP <- trunc(3 + sqrt(ncol(dataset)))
+  NS <- trunc(1 + sqrt(nrow(dataset)))
+  blockerLoop(dataset, sampleClades, proteinClades, NP, NS)
+}
+
 pivot <- function(results) {
   mat <- matrix(NA, nrow=max(results[,1]), ncol=max(results[,2]))
   for (i in 1:nrow(results)) {
@@ -42,12 +73,12 @@ pivot <- function(results) {
   mat
 }
 
-n2loglik <- function(x) {
+n2loglik <- function(x, nn) {
   xp <- x$ProteinGroups
   xs <- x$SampleGroups
   xe <- x$MeanMSE        # mean square residual
   kk <- xp*xs            # number of parameters
-  nn <- prod(dim(dat))   # number of observations
+#  nn <- prod(dim(dat))   # number of observations
   n2loglik <- nn*log(xe) # -2 log(likelihood)
   aic <- n2loglik + 2*kk
   bic <- n2loglik + kk*log(nn)
