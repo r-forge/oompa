2b33d8f10a131b75ac841f78aa6cfa7e072b2e82
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2022-09-23 08:32:45 +0000

    Adding classes and methods to combine a;; weights.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@561 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/plasma/R/05-interpret.R
+++ pkg/plasma/R/05-interpret.R
@@ -105,3 +105,39 @@ influencer <- function(object) {
   wolf <- lapply(toto, function(X) X@contrib)
   do.call(rbind, wolf)
 }
+
+setClass("CombinedWeights",
+         slots = c(combined = "matrix",
+                   featureSize = "numeric",
+                   dataSource = "factor"))
+
+combineAllWeights <- function(object) {
+  combined <- lapply(names(object@compModels), function(N) getAllWeights(object, N)@contrib)
+  names(combined) <- names(object@compModels)
+  featsize <- sapply(combined, nrow)
+  datasrc <- factor(rep(names(combined), times = featsize))
+  combined <- do.call(rbind, combined)
+  new("CombinedWeights",
+      combined = combined,
+      featureSize = featsize,
+      dataSource = datasrc)
+}
+
+## summary method for MultiOmics objects
+setMethod("summary", "CombinedWeights", function(object, ...) {
+  contra <- object@combined
+  temp <- list(object@dataSource)
+  aggie <- function(X, L, F) {
+    mu <- aggregate(X, L, F)
+    rownames(mu) <- mu[, 1]
+    mu$Group.1 <- NULL
+    mu <- as.matrix(mu)
+  }
+
+  mean <- aggie(contra, temp, mean)
+  sd <- aggie(contra, temp, sd)
+  med <- aggie(contra, temp, median)
+  mad <- aggie(contra, temp, mad)
+
+  list(Mean = mean, SD = sd, Median = med, MAD = mad)
+})
