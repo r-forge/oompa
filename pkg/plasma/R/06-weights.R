a79bf9884f0d2cd9269b75a824a9c843be7384f9
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2022-09-27 13:31:35 +0000

    Updated vignette, with new functions for interpretation of components.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@565 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/plasma/R/06-weights.R
+++ pkg/plasma/R/06-weights.R
@@ -34,3 +34,47 @@ setMethod("summary", "CombinedWeights", function(object, ...) {
 
   list(Mean = mean, SD = sd, Median = med, MAD = mad)
 })
+
+setMethod("image", "CombinedWeights", function(x, ...) {
+  summer <- summary(x)
+  mx <- max(abs(summer$Mean))
+  sx <- max(summer$SD)
+  dx <- max(abs(summer$Median))
+  ax <- max(summer$MAD)
+  N <- ncol(summer$Mean)
+  M <- nrow(summer$Mean)
+  opar <- par(mfrow = c(2,2))
+  on.exit(par(opar))
+  image(1:N, 1:M, t(summer$Mean), xlab = "Component", ylab = "Dataset",
+        main = "Mean", zlim = c(-mx, mx), col = redgreen(64))
+  image(1:N, 1:M, t(summer$SD), xlab = "Component", ylab = "Dataset",
+        main = "SD", zlim = c(0, sx), col = bluescale(64))
+  image(1:N, 1:M, t(summer$Median), xlab = "Component", ylab = "Dataset",
+        main = "Median", zlim =c(-dx, dx), col = redgreen(64))
+  image(1:N, 1:M, t(summer$MAD), xlab = "Component", ylab = "Dataset",
+        main = "Mad", zlim = c(0, ax),col = bluescale(64))
+  invisible(x)
+})
+
+stdize <- function(object, type = c("standard", "robust")) {
+  type <- match.arg(type)
+  summer <- summary(object)
+  mu <- switch(type,
+               standard = summer$Mean,
+               robust = summer$Median)
+  sigma <- switch(type,
+                  standard = summer$SD,
+                  robust = summer$MAD)
+  M <- apply(mu, 2, function(X) rep(X, times = object@featureSize))
+  S <- apply(sigma, 2, function(X) rep(X, times = object@featureSize))
+  (object@combined - M)/S
+}
+
+interpret <- function(object, component, alpha = 0.05) {
+  brute <- stdize(object)
+  Q <- qnorm(1 - alpha/2) # two-sided 5% cutoff
+  topA <- which(abs(brute[, component]) > Q)
+  data.frame(Feature = rownames(brute)[topA],
+             Source = object@dataSource[topA],
+             Weight = brute[topA, component])
+}
