37eadca03229645ee9a64659fac71bc5fb8b0807
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2024-08-17 15:04:20 +0000

    Updated make script to use spell check
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/oompa@659 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tools/makeRF.pl
+++ tools/makeRF.pl
@@ -38,6 +38,7 @@ if (defined($toolhash{HOME})) {
 $ENV{_R_CHECK_RD_NOTE_LOST_BRACES_} = "true";
 $ENV{_R_CHECK_XREFS_PKGS_ARE_DECLARED_} = "true";
 $ENV{_R_CHECK_CRAN_INCOMING_REMOTE_} = "true";
+$ENV{_R_CHECK_CRAN_INCOMING_USE_ASPELL_} = "true";
 
 my $profile =<<EOP
 r <- getOption("repos")
